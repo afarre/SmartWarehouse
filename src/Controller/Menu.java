f8c2d1750ef88295a78e82c8c5d2edb6eac6dd17
sartox <ls30927@salleurl.edu>
2017-12-16 12:40:27 +0100

    Corregidos errores en la configuracion del escenario.

--- src/Controller/Menu.java
+++ src/Controller/Menu.java
@@ -24,7 +24,7 @@ public class Menu {
      * Permite la eleccion de las dintintas opciones del menu comprendidas entre 1 y 5 incluidas, con control de erorres
      */
     public void mostraMenu() {
-        int i = 0;
+        int i;
         do {
             System.out.println("\n1. Configurar magatzem");
             System.out.println("2. Carregar productes");
@@ -33,11 +33,11 @@ public class Menu {
             System.out.println("5. Sortir\n");
             System.out.println("\nSel·lecciona una opcio:");
 
-            i = readInt(i);
+            i = readInt();
 
             while (i < 1 || i > 5) {
                 System.out.println("Opcio del menu incorrecta! Introdueix l'opcio de nou:");
-                i = readInt(i);
+                i = readInt();
             }
 
             switch (i){
@@ -83,8 +83,19 @@ public class Menu {
         boolean map[][] = new boolean[configJson.get("dim").getAsJsonObject().get("max_x").getAsInt()][configJson.get("dim").getAsJsonObject().get("max_y").getAsInt()];
 
         for (int i = 0; i < configJson.get("shelves").getAsJsonArray().size(); i++){
-            map[configJson.get("shelves").getAsJsonArray().get(i).getAsJsonObject().get("x_start").getAsInt()][configJson.get("shelves").getAsJsonArray().get(i).getAsJsonObject().get("y_start").getAsInt()] = false;
-
+            for (int j = 0; j < configJson.get("shelves_config").getAsJsonArray().size(); j++){
+                if (configJson.get("shelves_config").getAsJsonArray().get(j).getAsJsonObject().get("id").getAsInt() == configJson.get("shelves").getAsJsonArray().get(i).getAsJsonObject().get("config").getAsInt()){
+                    if (configJson.get("shelves").getAsJsonArray().get(i).getAsJsonObject().get("orientation").getAsString().equals("V")){
+                        for (int k = 0; k < configJson.get("shelves_config").getAsJsonArray().get(j).getAsJsonObject().get("length").getAsInt(); k++){
+                            map[configJson.get("shelves").getAsJsonArray().get(i).getAsJsonObject().get("x_start").getAsInt()][configJson.get("shelves").getAsJsonArray().get(i).getAsJsonObject().get("y_start").getAsInt() + k] = true;
+                        }
+                    } else {
+                        for (int k = 0; k < configJson.get("shelves_config").getAsJsonArray().get(j).getAsJsonObject().get("length").getAsInt(); k++){
+                            map[configJson.get("shelves").getAsJsonArray().get(i).getAsJsonObject().get("x_start").getAsInt() + k][configJson.get("shelves").getAsJsonArray().get(i).getAsJsonObject().get("y_start").getAsInt()] = true;
+                        }
+                    }
+                }
+            }
         }
 
         WarehouseView warehouseView = new WarehouseView(map, configJson.get("entrance").getAsJsonObject().get("x").getAsInt(), configJson.get("entrance").getAsJsonObject().get("y").getAsInt());
@@ -150,10 +161,10 @@ public class Menu {
 
     }
 
-    private int readInt(int i){
+    private int readInt(){
         try {
             Scanner read = new Scanner(System.in);
-            i = read.nextInt();
+            int i = read.nextInt();
             return i;
         }catch (InputMismatchException ignored){
         }
--- src/View/WarehouseView.java
+++ src/View/WarehouseView.java
@@ -1,7 +1,5 @@
 package View;
 
-import Controller.BoxListener;
-
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.MouseListener;
